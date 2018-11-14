bcf5ef404d2474136e1a1045f1884553b954a360
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-11-13 21:15:39 -0500

    added employeelist and started working on dom.js file

---
+++ employeedirectory/dom.js
@@ -0,0 +1,15 @@
+const $ = function (selector) {
+    const nodeList = document.querySelectorAll(selector);
+  
+    const text = function(content){
+      for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].innerText = content;
+      }
+    }
+    
+    const html = function(content){
+      for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].innerHTML = content;
+      }
+    }
+}  
---
+++ employeedirectory/employeeList.js
@@ -0,0 +1,21 @@
+const listOfEmployees = [{
+    name: 'Adit',
+    officeNum: 12,
+    phoneNum: '222-233-3422'
+  },
+  {
+    name: 'Johnn',
+    officeNum: 4,
+    phoneNum: '489-234-5324'
+  },
+  {
+    name: 'Michelle',
+    officeNum: 89,
+    phoneNum: '234-643-7234'
+  },
+  {
+    name: 'Roger',
+    officeNum: 52,
+    phoneNum: '111-756-4354'
+  }
+]
